d16ff01e96ed5192fc791b0a91a117f0f6aa9b4a
Victor Guedes <mirandaguedes.victor@gmail.com>
2021-09-14 21:26:09 -0300

    Add test valid password confirmation and mandatory fields

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,19 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-end
+  test "valid password confirmation" do
+   tester = User.new
+   tester.email = 'tester@dragonrentals.com'
+   tester.username = 'tester'
+   tester.password = '123456'
+   tester.password_confirmation = '123456!!'
+   assert_not tester.valid?
+  end
+
+  test "blank mandatory fields" do
+    tester = User.new
+    assert_not tester.valid?
+   end
+
+
+end 
