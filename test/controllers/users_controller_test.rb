9cb57bd944256d932f6ccbd77f509ef60d087e6d
Franco Caldas <francocaldas@gmail.com>
2021-09-16 20:25:42 -0300

    altera  o teste para exclusão de usuário

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,11 +1,19 @@
 require "test_helper"
+
 class UsersControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
+  def setup
+    @user = users(:one)
+  end
+
   test "should destroy user" do
-    sign_in users(:one)
-    
-    delete user_registration_url
-    
+    sign_in(@user)
+
+    assert_difference 'User.count', -1 do
+      
+      delete user_registration_url
+    end
+
     assert_redirected_to root_url
   end
 end
