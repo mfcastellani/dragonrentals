b26fcfdd923d3c578f161d5ee8d4ae10ac610cbe
Paulo Lins <64275070+pauloalchemist@users.noreply.github.com>
2021-10-02 16:37:47 -0300

    added bootstrap in project (#27)

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -6,6 +6,7 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
+import '../js/bootstrap_js_files.js'  
 
 Rails.start()
 Turbolinks.start()
