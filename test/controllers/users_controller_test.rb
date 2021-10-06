327bb40389340f59e6da5b669cb78b46e095a93d
Paulo Castro <pcastro3325@gmail.com>
2021-10-05 22:35:30 -0300

    Include new validade tests
    Foram feitos 2 testes, no segundo, o teste de dados inválidos,
    a resposta no site, ao entrar com dados inválidos foi de 200,
    para emular no caso de erro, utilizamos o mesmo parâmetro
     e a resposta foi de 200 também, dessa forma, fica aberta a discussão
     para os demais membros.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -19,10 +19,23 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should update user" do
     sign_in(@user)
-
-    patch user_registration_url, params: { user: { email: @user.email, username: 'banana', current_password: '123456' } }
+    new_user_mail = 'useremail@gmail.com'
+    
+    patch user_registration_url, params: { user: { email: new_user_mail, username: 'banana', current_password: '123456' } }
 
     assert_redirected_to root_url
+
+    @user.reload
+
+    assert_equal @user.email, new_user_mail
   end
   
+  test "should not update user" do
+    sign_in(@user)
+
+    patch user_registration_url, params: { user: {email: 'useremailgmail.com', username: 'batata', current_password: '123456'}}
+    
+    assert_response :success
+
+  end
 end
